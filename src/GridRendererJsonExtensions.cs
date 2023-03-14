c266aaefad2a582ad3e30185e4b65b91bd2c1ffd
Vladyslav Zaiets <rutova2@gmail.com>
2023-03-14 07:31:08 +0200

    Wip

--- src/GridRendererJsonExtensions.cs
+++ src/GridRendererJsonExtensions.cs
@@ -3,17 +3,29 @@ using System.Text.Json.Serialization;
 
 namespace SkiaScope;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="GridRenderer"/>.
+/// </summary>
 public static class GridRendererJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
     {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         WriteIndented = false,
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
     };
 
+    /// <summary>
+    /// Serializes a <see cref="GridRenderer"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The <see cref="GridRenderer"/> instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the <see cref="GridRenderer"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
     public static string ToJson(this GridRenderer value, bool indented = false)
     {
+        ArgumentNullException.ThrowIfNull(value);
+
         var options = indented
             ? new JsonSerializerOptions(_jsonOptions)
             {
@@ -24,13 +36,30 @@ public static class GridRendererJsonExtensions
         return JsonSerializer.Serialize(value, options);
     }
 
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="GridRenderer"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized <see cref="GridRenderer"/> instance, or <see langword="null"/> if the JSON is invalid.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
     public static GridRenderer? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         return JsonSerializer.Deserialize<GridRenderer>(json, _jsonOptions);
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="GridRenderer"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized <see cref="GridRenderer"/> instance if successful; otherwise, <see langword="null"/>.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
     public static bool TryFromJson(string json, out GridRenderer? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         try
         {
             value = JsonSerializer.Deserialize<GridRenderer>(json, _jsonOptions);
